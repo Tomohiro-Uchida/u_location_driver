cbc5610b02b0da0c0d01dc38e305e2e5a68371d5
Tomohiro-Uchida <uchida001tmhr@gmail.com>
2025-09-28 17:27:14 +0900

    0.4.0+100

--- android/src/main/kotlin/com/jimdo/uchida001tmhr/u_location_driver/ULocationDriverPlugin.kt
+++ android/src/main/kotlin/com/jimdo/uchida001tmhr/u_location_driver/ULocationDriverPlugin.kt
@@ -158,7 +158,6 @@ class ULocationDriverPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, D
             println("informLocationToDart: notImplemented")
           }
         })
-      } else {
       }
     }
 
